22f2c5cf8037dcd3b2f2c452df10864601e8c330
박철현 <tghy4@naver.com>
2024-04-29 23:02:31 +0900

    1.Servlet의 예외처리 학습 및 내용 정리

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -13,21 +13,14 @@ public class ServletExController {
 
     @GetMapping("/error-ex")
     public void errorEx() {
-        throw new RuntimeException("예외 발생!");
+        throw new RuntimeException("RuntimeException!!!");
     }
-
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
-        response.sendError(404, "404 오류!");
+        response.sendError(HttpServletResponse.SC_NOT_FOUND,"404 Error!!");
     }
-
-    @GetMapping("/error-400")
-    public void error400(HttpServletResponse response) throws IOException {
-        response.sendError(400, "400 오류!");
-    }
-
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
-        response.sendError(500);
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"500 Error!!");
     }
 }
